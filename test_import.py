fce0d7f7b400ca6ceed2526cb64f7e13f3a76c6e
ThanhNhut1404 <huynhthanhnhut144@gmail.com>
2025-06-10 13:59:44 +0700

    Test import thư viện

---
+++ test_import.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+from PIL import Image
+import imutils
+import dlib
+import face_recognition
+
+print("✅ OpenCV:", cv2.__version__)
+print("✅ NumPy:", np.__version__)
+print("✅ Pillow:", Image.__version__)
+print("✅ Imutils:", imutils.__version__)
+print("✅ dlib:", dlib.__version__)
+print("✅ face_recognition:", face_recognition.__version__)
